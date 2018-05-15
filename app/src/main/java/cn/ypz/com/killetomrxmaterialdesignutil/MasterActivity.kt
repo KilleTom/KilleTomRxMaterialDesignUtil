c1fd8a4640cdb5d7d0ff7e7d23924a3491541c0f
易庞宙 <1986545332@qq.com>
2018-05-15 18:34:50 +0800

    Initial commit

---
+++ app/src/main/java/cn/ypz/com/killetomrxmaterialdesignutil/MasterActivity.kt
@@ -0,0 +1,12 @@
+package cn.ypz.com.killetomrxmaterialdesignutil
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MasterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_master)
+    }
+}
