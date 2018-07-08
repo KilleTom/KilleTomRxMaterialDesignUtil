b493507279a0f5071105a38a3d7c5cbf74660ade
易庞宙 <1986545332@qq.com>
2018-07-08 11:25:56 +0800

    增加运行结果程序GIF效果图

--- app/src/main/java/cn/ypz/com/killetomrxmaterialdesignutil/PermissionDIYActivity.kt
+++ app/src/main/java/cn/ypz/com/killetomrxmaterialdesignutil/PermissionDIYActivity.kt
@@ -8,55 +8,50 @@ import cn.ypz.com.killetomrxmateria.rxwidget.permissiondialog.RxPermissionEmpty
 import cn.ypz.com.killetomrxmateria.rxwidget.permissiondialog.RxPermissions
 import cn.ypz.com.killetomrxmateria.rxwidget.toast.RxToast
 import cn.ypz.com.killetomrxmateria.rxwidget.toast.anim.RxToastType
+import kotlinx.android.synthetic.main.activity_permission_diy.*
 
 class PermissionDIYActivity : AppCompatActivity(), RxPermissions.Builder.PermissionDialogCancle, RxPermissions.Builder.RequestpermissionSelf {
 
     override fun self() = RxToast.Config.getInstance().show(RxToastType.RxToastInfoType, this, "已经存在权限").apply()
 
-    override fun cancle() {
-        RxToast.Config.getInstance().show(RxToastType.RxToastWarningType, this, "弹窗权限取消").apply()
-    }
+    override fun cancle() = RxToast.Config.getInstance().show(RxToastType.RxToastWarningType, this, "弹窗权限取消").apply()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_permission_diy)
-    }
-
-
-    private fun dialog() {
-        RxPermissions.with(this).initDialogPermission(
-                RxPermissions.Builder.RequestpermissionSelf { this.self() },
-                RxPermissions.Builder.PermissionDialogCancle { this.cancle() },
-                RxPermissionEmpty(Manifest.permission.WRITE_EXTERNAL_STORAGE, "文件", R.mipmap.rx_permission_calendar_icon),
-                RxPermissionEmpty(Manifest.permission.CAMERA, "相机", R.mipmap.rx_permission_camera_icon),
-                RxPermissionEmpty(Manifest.permission.READ_EXTERNAL_STORAGE, "文件", R.mipmap.rx_permission_calendar_icon),
-                RxPermissionEmpty(Manifest.permission.CALL_PHONE, "电话", R.mipmap.rx_permission_camera_icon),
-                RxPermissionEmpty(Manifest.permission.READ_SMS, "短信", R.mipmap.rx_permission_calendar_icon),
-                RxPermissionEmpty(Manifest.permission.SEND_SMS, "短信", R.mipmap.rx_permission_camera_icon)
-        ).build()
-    }
-
-    private fun noDialog1() {
-        RxPermissions.with(this)
-                .addPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                .addPermission(Manifest.permission.CAMERA)
-                .initPermission { this.self() }
-    }
-
-    private fun noDialog2() {
-        RxPermissions.with(this)
-                .baseNoDialogInitPerission(
-                        RxPermissions.Builder.RequestpermissionSelf { this.self() },
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                        Manifest.permission.CAMERA,
-                        Manifest.permission.CALL_PHONE
-                )
+        p_dialog.setOnClickListener {
+            RxPermissions.with(this).initDialogPermission(
+                    RxPermissions.Builder.RequestpermissionSelf { this.self() },
+                    RxPermissions.Builder.PermissionDialogCancle { this.cancle() },
+                    RxPermissionEmpty(Manifest.permission.WRITE_EXTERNAL_STORAGE, "文件", R.mipmap.rx_permission_calendar_icon),
+                    RxPermissionEmpty(Manifest.permission.CAMERA, "相机", R.mipmap.rx_permission_camera_icon),
+                    RxPermissionEmpty(Manifest.permission.READ_EXTERNAL_STORAGE, "文件", R.mipmap.rx_permission_calendar_icon),
+                    RxPermissionEmpty(Manifest.permission.CALL_PHONE, "电话", R.mipmap.rx_permission_camera_icon),
+                    RxPermissionEmpty(Manifest.permission.READ_SMS, "短信", R.mipmap.rx_permission_calendar_icon),
+                    RxPermissionEmpty(Manifest.permission.SEND_SMS, "短信", R.mipmap.rx_permission_camera_icon)
+            ).build()
+        }
+        p_1.setOnClickListener {
+            RxPermissions.with(this)
+                    .addPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    .addPermission(Manifest.permission.CAMERA)
+                    .initPermission { this.self() }
+        }
+        p_2.setOnClickListener {
+            RxPermissions.with(this)
+                    .baseNoDialogInitPerission(
+                            RxPermissions.Builder.RequestpermissionSelf { this.self() },
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.CAMERA,
+                            Manifest.permission.CALL_PHONE
+                    )
+        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == 1)
-            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
                 RxToast.Config.getInstance().show(RxToastType.RxToastSuccessType, this, "获取到权限了").apply()
             else
                 RxToast.Config.getInstance().show(RxToastType.RxToastWarningType, this, "有些权限用户没有允许").apply()
